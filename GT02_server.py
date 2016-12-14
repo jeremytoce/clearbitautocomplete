06f861c14a94bced174d5eb2ac7fb0023c3ea74e
Jeremy Toce <jeremytoce@gmail.com>
2016-12-14 15:28:13 -0800

    Update GT02_server.py

--- GT02_server.py
+++ GT02_server.py
@@ -9,8 +9,8 @@ import suffixes as suffixes
 ### Configuration
 ##################
 
-clearbit_key = 'sk_885bdf7352ae3c6a36b9b3266072c184'
-bing_key = 'CU79dfqZfHOL0dYHwo8RLkN60TRl4Vf8FQhoVj//Dt0'
+clearbit_key = ''
+bing_key = ''
 
 
 #######################################
